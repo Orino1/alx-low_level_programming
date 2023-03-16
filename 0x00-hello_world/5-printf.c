ccd82a6d4397a98f48d999ccfa06359107307c06
Orino1 <orinomohamed@gmail.com>
2023-03-16 10:20:47 -0700

    adding 5-printf.c file after fixing tons of warning from betty

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -  Function main will print a string of char
+ * printf is function that print a string of char
+ * Return: a value of 0 which mean the program run successfully
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
