e4e751ba616995cb7b3d3ccecc72d34f74c52076
Orino1 <orinomohamed@gmail.com>
2023-03-16 16:17:41 -0700

    printing to the stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - will print a quote to the standard error
+ * Return: will return 1 as the program runs successfully
+*/
+int main(void)
+{
+    fprintf(stderr ,"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+    return (1);
+}
