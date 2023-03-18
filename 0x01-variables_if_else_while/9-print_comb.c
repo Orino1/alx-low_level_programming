3d9ed96919232dcbca180e6e3a69dd733ac75e80
Orino1 <orinomohamed@gmail.com>
2023-03-17 17:51:35 -0700

    ommmmg the last one is also perfect (i hope so lol)

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - A function all single digit
+ * numbers of base 10 starting from 0
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 0;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar('0' + num);
+		if (num != 9)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	return (0);
+}
