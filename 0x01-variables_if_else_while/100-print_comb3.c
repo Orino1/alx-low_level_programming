2a1496ff4de756fe279b2f2293769928564a043a
Orino1 <orinomohamed@gmail.com>
2023-03-17 18:37:03 -0700

    thsi is the best code i have ever written

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - A function that prints all possible
+ * different combinations of two digits
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int numFirst = 0;
+
+	int numSecond = 0;
+
+	int stepdownum = 0;
+
+	for (numFirst = 0; numFirst <= 9; numFirst++)
+	{
+		for (numSecond = stepdownum; numSecond <= 9; numSecond++)
+		{
+			if (numFirst != numSecond)
+			{
+				putchar('0' + numFirst);
+				putchar('0' + numSecond);
+				if (numFirst != 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				else if (numFirst == 89)
+				{
+					putchar('\n');
+				}
+			}
+		}
+		stepdownum++;
+	}
+	putchar('\n');
+	return (0);
+}
