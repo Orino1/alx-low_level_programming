e8f71add38cc880859e84b2958b0ca77bda81788
Orino1 <orinomohamed@gmail.com>
2023-03-21 08:47:40 -0700

    adding a descriptiong fot the func

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - A function that prints From a to z
+ * print_alphabet-x10 A function that prints From a to z
  * 10x followed by a new line
  * Return: Always 0 (Success)
  */
@@ -11,7 +11,7 @@ void print_alphabet_x10(void)
 
 		int i;
 
-		for ( i = 0; i < 10; i++)
+		for (i = 0 ; i < 10; i++)
 		{
 			for (str = 'a'; str <= 'z'; str++)
 			{
