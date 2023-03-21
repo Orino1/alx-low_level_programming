efda2fce8f0c7cfafa9f22e57a9e21b96a6c3947
Orino1 <orinomohamed@gmail.com>
2023-03-21 14:00:50 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,12 +14,12 @@ int print_last_digit(int n)
 
 		int lastn = pos % 10;
 
-		return (lastn);
+		return (lastn, lastn);
 	}
 	else
 	{
 		int lastn = n % 10;
 
-		return (lastn);
+		return (lastn, lastn);
 	}
 }
