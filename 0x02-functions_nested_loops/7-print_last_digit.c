fa7663b21f8d9fdb73f2a5443e528acf11a4d689
Orino1 <orinomohamed@gmail.com>
2023-03-21 13:39:40 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - A function function that
+ * prints the last digit of a number.
+ * @n: is a number
+ * Return: n or sum
+ */
+int print_last_digit(int n)
+{
+	int lastn = n % 10;
+
+	return (lastn);
+}
