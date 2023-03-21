7b4c82ccdde80596cda41b36e53042e63cc4a208
Orino1 <orinomohamed@gmail.com>
2023-03-21 13:09:50 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - A function function that 
+ * prints the sign of a number.
+ * @n: is a iether a variable or degit
+ * Return: 1 mean + 0 mean 0 -1 mean - sign
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
