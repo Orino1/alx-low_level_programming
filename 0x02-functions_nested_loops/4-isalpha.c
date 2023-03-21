4380b5f48a667092312ad86effdd244c8011617d
Orino1 <orinomohamed@gmail.com>
2023-03-21 12:30:58 -0700

    4-isalpha.c file

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _islower - A function that check if c
+ * is lowercase or uppercase char
+ * @c: is a iether a variable or degit
+ * Return: Always 0 (Success)
+ */
+int _isalpha(char c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
