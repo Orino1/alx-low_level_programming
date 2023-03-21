67ea4d09e4731c12aeca450b656ecd86334a405d
Orino1 <orinomohamed@gmail.com>
2023-03-21 14:40:02 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -23,14 +23,3 @@ int print_last_digit(int n)
 		return (lastn);
 	}
 }
-int main(void)
-{
-    int r;
-
-    print_last_digit(98);
-    print_last_digit(0);
-    r = print_last_digit(-1024);
-    _putchar('0' + r);
-    _putchar('\n');
-    return (0);
-}
