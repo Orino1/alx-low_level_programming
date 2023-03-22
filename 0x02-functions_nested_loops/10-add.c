31ca65f9a566ded6e9a36f271cb72bd8f59376eb
Orino1 <orinomohamed@gmail.com>
2023-03-21 17:05:36 -0700

    pushiing a simple function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - A function function that
+ * adds two num.
+ * @one: is a number
+ * @two: is a number
+ * Return: lastn
+ */
+int add(int one, int two)
+{
+	int sum = one + two;
+
+	return (sum);
+}
