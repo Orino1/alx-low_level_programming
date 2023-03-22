161518e46e82bb36445b64667d21a145f97e8cc8
Orino1 <orinomohamed@gmail.com>
2023-03-22 09:07:15 -0700

    first files

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - A function function that
+ * call another function
+ * Return: always (0)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
