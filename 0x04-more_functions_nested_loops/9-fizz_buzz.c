c01ee3e1ac425f9bdd54f6d0a37ec6f341e4c0ef
Orino1 <orinomohamed@gmail.com>
2023-03-23 15:51:48 -0700

    perfect function

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - numbers from 1 to 100,
+ * followed by a new line.
+ * Return: always 0 as sucs
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3) == 0 && (i % 5) == 0)
+		{
+			printf("FizzBuzz");
+			if (i != 100)
+				printf(" ");
+		}
+		if ((i % 3) == 0)
+		{
+			printf("Fizz");
+			if (i != 100)
+				printf(" ");
+		}
+		else if ((i % 5) == 0)
+		{
+			printf("Buzz");
+			if (i != 100)
+				printf(" ");
+		}
+		else
+		{
+			printf("%d", i);
+			if (i != 100)
+				printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
