2f1fdedd48e3b2342b88f684d450cb87b3edf854
Orino1 <orinomohamed@gmail.com>
2023-03-23 16:13:02 -0700

    adding else before if

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,19 +12,19 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if ((i % 3) == 0 && (i % 5) == 0)
 		{
 			printf("FizzBuzz");
 			if (i != 100)
 				printf(" ");
 		}
-		if (i % 3 == 0 && i % 5 != 0)
+		else if ((i % 3) == 0 && (i % 5) != 0)
 		{
 			printf("Fizz");
 			if (i != 100)
 				printf(" ");
 		}
-		else if (i % 5 == 0 && i % 3 != 0)
+		else if ((i % 5) == 0 && (i % 3) != 0)
 		{
 			printf("Buzz");
 			if (i != 100)
