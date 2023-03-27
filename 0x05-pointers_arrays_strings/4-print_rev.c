9e6cfca357993e57c3bb7674a085bd29e27737dd
Orino1 <orinomohamed@gmail.com>
2023-03-27 13:28:46 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - function that print a str
+ * to stdout in revs
+ * @s: string of char
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	int leng = 0;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		leng++;
+	}
+	for (i = leng; i >= 0; i--)
+	{
+		_putchar(*(s + i));
+	}
+	_putchar('\n');
+}
