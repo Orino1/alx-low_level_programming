c2859a7697eedad29eee1694bf0308c4a6d0673b
Orino1 <orinomohamed@gmail.com>
2023-03-27 09:07:23 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - function that take a pointer
+ * and chane the value of the
+ * vr that it points to to 98
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
