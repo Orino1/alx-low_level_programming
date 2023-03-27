a9a53aec5ad0482c625ff7db616e1574acfc828d
Orino1 <orinomohamed@gmail.com>
2023-03-27 09:25:10 -0700

    pushing a function that return the lengt of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - function that return
+ * the len of str
+ * @s: string of char
+ * Return: len
+ */
+int _strlen(char *s)
+{
+	int lengt = 0;
+
+	while (*s != '\0')
+	{
+		lengt++;
+	}
+	return (lengt);
+}
