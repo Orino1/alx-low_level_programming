8777e436014899cb0f542fa36adf23d853fa6c92
Orino1 <orinomohamed@gmail.com>
2023-03-27 14:24:32 -0700

    function that reverse a string of char

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * rev_string - function that reverse
+ * a string of char
+ * @s: string of char
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i;
+
+	int leng = 0;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		leng++;
+	}
+	int start = 0;
+
+	int start2 = 0;
+
+	char test[leng - 1];
+
+	for (i = leng - 1; i >= 0; i--)
+	{
+		test[start] = s[i];
+		start++;
+	}
+	for (i = leng - 1; i >= 0; i--)
+	{
+		s[start2] = test[start2];
+		start2++;
+	}
+}
