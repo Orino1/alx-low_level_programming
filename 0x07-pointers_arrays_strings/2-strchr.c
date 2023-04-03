cb4df9b798c6715287e50f1fa1183742b9a06b70
Orino1 <orinomohamed@gmail.com>
2023-04-03 02:13:11 -0700

    dfine null

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stddef.h>
 /**
  * _strchr - Write a function that locates a
  * character in a string
@@ -11,6 +10,8 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
+	char x = NULL;
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
@@ -18,5 +19,5 @@ char *_strchr(char *s, char c)
 			return (s + i);
 		}
 	}
-	return (NULL);
+	return (x);
 }
