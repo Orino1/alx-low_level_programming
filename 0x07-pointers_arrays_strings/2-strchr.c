d411830aca7be8b7e522b58e0d48dd2679530159
Orino1 <orinomohamed@gmail.com>
2023-04-02 21:31:09 -0700

    uploading 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strchr - Write a function that locates a
+ * character in a string
+ * @s: pointer to string
+ * @c: char
+ * Return: pointer dest
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return (NULL);
+}
