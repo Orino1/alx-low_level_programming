06cb97b166ef5d0554bfa2e2aee834f119558895
Orino1 <orinomohamed@gmail.com>
2023-04-09 03:30:25 -0700

    uploading 1-args.c file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - A function that print argc
+ * @argc: arg count
+ * @argv: agr vector / array
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char const *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
