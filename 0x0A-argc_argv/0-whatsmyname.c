f4e75b85266845510da61bdd2b295d7bf0be8676
user <user@192.168.1.8>
2023-04-09 10:16:40 +0000

    uploading 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - A function that print the program name
+ * @argc: arg count
+ * @argv: agr vector / array
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char const *argv[])
+{
+        printf("%s\n",argv[0]);
+    return 0;
+}
