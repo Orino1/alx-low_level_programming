ee42a48c36a56e94abfd91ddfd61539087fb1c33
Orino1 <orinomohamed@gmail.com>
2023-04-13 03:39:26 -0700

    Fixing an error (Comparing un int to int)

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -5,12 +5,11 @@
  * string_nconcat - a function that concatenates two strings.
  * @s1: string 1
  * @s2: string 2
- * @n: Number of bytes
  * Return: VOID pointer
  */
-int lengo(char *str)
+unsigned int lengo(char *str)
 {
-	int i = 0;
+	unsigned int i = 0;
 
 	while (str[i] != '\0' && str != NULL)
 	{
@@ -21,7 +20,8 @@ int lengo(char *str)
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
-	int i, j = 0;
+	unsigned int i = 0;
+	unsigned int j = 0;
 
 	if (s2 != NULL && n >= lengo(s2))
 	{
@@ -44,11 +44,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		p[i] = s1[i];
 		i++;
 	}
-	while (s2 != NULL && j < lengo(s2))
+	while (s2 != NULL && j < n)
 	{
-		p[i + j] = s2[i];
+		p[lengo(s1) + j] = s2[j];
 		j++;
 	}
-	p[i + j] = '\0';
+	p[lengo(s1) + j] = '\0';
 	return (p);
 }
