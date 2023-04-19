cff00e38caa80c759ea34eefd1e8c9f389f67bfd
Orino1 <orinomohamed@gmail.com>
2023-04-19 10:20:26 -0700

    Quik fix

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 
 /**
  * int_index - a function that searches for an integer.
@@ -16,11 +17,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		return (-1);
 	}
-	for (i = 0; i < size; i++)
+	if (array != NULL && array != NULL)
 	{
-		if ((cmp(array[i])) != 0)
+		for (i = 0; i < size; i++)
 		{
-			return (i);
+			if ((cmp(array[i])) != 0)
+			{
+				return (i);
+			}
 		}
 	}
 	return (-1);
