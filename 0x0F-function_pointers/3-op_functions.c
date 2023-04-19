1662e92e72bbea71d9fb5efa4ecd42791c16d760
Orino1 <orinomohamed@gmail.com>
2023-04-19 11:27:38 -0700

    Uploading files

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - function that adds two numbers
+ * @a: number.
+ * @b: number
+ * Return: a + b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - function sub a num from a nother
+ * @a: number.
+ * @b: number
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - function that multp two numbesrs
+ * @a: number.
+ * @b: number
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - function sub a num from a nother
+ * @a: number.
+ * @b: number
+ * Return: a / b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - function sub a num from a nother
+ * @a: number.
+ * @b: number
+ * Return: a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
