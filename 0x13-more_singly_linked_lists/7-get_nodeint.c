8ff5e4804be619070a5037a65ce09c198eabc4fe
Orino1 <orinomohamed@gmail.com>
2023-04-30 16:15:48 -0700

    fixing betty style

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,8 +12,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 	listint_t *temporary;
-	temporary = head;
 
+	temporary = head;
 	if (temporary == NULL)
 	{
 		return (NULL);
