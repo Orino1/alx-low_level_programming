2bd860fcc4c412407ce264f8c0885003b0e40d56
Orino1 <orinomohamed@gmail.com>
2023-04-30 15:38:32 -0700

    adding a temp pointer to store the next pointer

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,9 +8,12 @@
  */
 void free_listint(listint_t *head)
 {
+	listint_t *temporary;
+
 	while (head != NULL)
 	{
+		temporary = head->next
 		free(head);
-		head = head->next;
+		head = temporary;
 	}
 }
