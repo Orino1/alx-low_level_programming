11c9a3de04360015d9bc07f1d813a48e74d8257f
Orino1 <orinomohamed@gmail.com>
2023-05-08 04:21:02 -0700

    fixing the compiler

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,31 +3,34 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
-#include "main.h"
 #include <sys/stat.h>
+#include "main.h"
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int bytes_read, bytes_written, fd;
+	char *buf;
+
 	if (filename == NULL) {
 		return (0);
 	}
-	int fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1) {
 		return (0);
 	}
-	char *buf = malloc(letters + 1);
+	buf = malloc(letters + 1);
 	if (buf == NULL) {
 		close(fd);
 		return (0);
 	}
-	int bytes_read = read(fd, buf, letters);
+	bytes_read = read(fd, buf, letters);
 	if (bytes_read == -1) {
 		free(buf);
 		close(fd);
 		return (0);
 	}
 	buf[bytes_read] = '\0';
-	int bytes_written = write(STDOUT_FILENO, buf, bytes_read);
+	bytes_written = write(STDOUT_FILENO, buf, bytes_read);
 	if (bytes_written == -1 || bytes_written != bytes_read) {
 		free(buf);
 		close(fd);
