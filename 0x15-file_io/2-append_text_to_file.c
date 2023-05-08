d481044fd508bf60928de5b01743a1fbb316b36a
Orino1 <orinomohamed@gmail.com>
2023-05-08 06:08:21 -0700

    test nm3

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include "main.h"
+#include <string.h>
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, len, bytes_written;
+
+	if (filename == NULL) {
+		return (-1);
+	}
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == (-1)) {
+		return (-1);
+	}
+	if (text_content != NULL) {
+		len = strlen(text_content);
+ 		bytes_written = write(fd, text_content, len);
+		if (bytes_written == -1 || bytes_written != len) {
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
