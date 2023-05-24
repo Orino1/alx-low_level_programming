5ef20477358a7ec66814f2382a12d1b9cad5111d
Orino1 <orinomohamed@gmail.com>
2023-05-24 16:27:30 -0700

    Fixing betty style

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,28 +7,36 @@
 #include "main.h"
 #include <string.h>
 
+/**
+ * create_file - test
+ *@filename: the file name
+ *@text_content: the text
+ * Return: Always 0.
+ */
+
 int create_file(const char *filename, char *text_content)
 {
 	int fd, len, bytes_written;
 
-	if (filename == NULL) {
+	if (filename == NULL)
+	{
 		return (-1);
 	}
-
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd == (-1)) {
+	if (fd == (-1))
+	{
 		return (-1);
 	}
-
-	if (text_content != NULL) {
+	if (text_content != NULL)
+	{
 		len = strlen(text_content);
- 		bytes_written = write(fd, text_content, len);
-		if (bytes_written == -1 || bytes_written != len) {
+		bytes_written = write(fd, text_content, len);
+		if (bytes_written == -1 || bytes_written != len)
+		{
 			close(fd);
 			return (-1);
 		}
 	}
-
 	close(fd);
 	return (1);
 }
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,22 +7,32 @@
 #include "main.h"
 #include <string.h>
 
+/**
+ * append_text_to_file - test
+ *@filename: the file name
+ *@text_content: the text
+ * Return: Always 0.
+ */
+
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, len, bytes_written;
 
-	if (filename == NULL) {
+	if (filename == NULL)
+	{
 		return (-1);
 	}
-
 	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd == (-1)) {
+	if (fd == (-1))
+	{
 		return (-1);
 	}
-	if (text_content != NULL) {
+	if (text_content != NULL)
+	{
 		len = strlen(text_content);
- 		bytes_written = write(fd, text_content, len);
-		if (bytes_written == -1 || bytes_written != len) {
+		bytes_written = write(fd, text_content, len);
+		if (bytes_written == -1 || bytes_written != len)
+		{
 			close(fd);
 			return (-1);
 		}
